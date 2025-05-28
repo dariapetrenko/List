ca599b76e983c77da4b6748acca3b3ef83c39c6b
dariapetrenko <D.Petrenko_FIT_2_22_B_d@knute.edu.ua>
2025-05-29 00:32:21 +0300

    add room functionality

--- app/src/main/java/softserve/academy/mylist/MainActivity.kt
+++ app/src/main/java/softserve/academy/mylist/MainActivity.kt
@@ -1,5 +1,7 @@
 package softserve.academy.mylist
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -27,12 +29,29 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.Role.Companion.Checkbox
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Update
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import softserve.academy.mylist.ui.theme.ListTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,23 +72,83 @@ class MainActivity : ComponentActivity() {
 
 // Model View ViewModel
 
+@Entity(tableName = "shopping_items")
 data class ShoppingItem(
     val name: String,
-    val isBought: Boolean = false
+    val isBought: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
 )
+//ORM
+@Dao
+interface ShoppingDao {
+    @Query("SELECT * FROM shopping_items")
+    fun getALLItems(): List<ShoppingItem>
 
-class ShoppingListViewModel : ViewModel() {
-    val shoppingList = mutableStateListOf(
-        ShoppingItem("Молоко"),
-        ShoppingItem("Хліб"),
-        ShoppingItem("Яблука"),
-        ShoppingItem("Олія"),
-        ShoppingItem("Рис"),
-    )
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertItem(item: ShoppingItem)
+
+    @Update
+    fun updateItem(item: ShoppingItem)
+
+    @Delete
+    fun deleteItem(item: ShoppingItem)
+}
+
+@Database(entities = [ShoppingItem::class], version = 1)
+abstract class ShoppingDatabase : RoomDatabase() {
+    abstract fun shoppingDao(): ShoppingDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ShoppingDatabase? = null
+
+        fun getInstance(context: Context): ShoppingDatabase {
+            return  INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ShoppingDatabase::class.java,
+                    "shopping_db"
+                ) .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
+class ShoppingListViewModel(application: Application) : AndroidViewModel(application) {
+    private val dao: ShoppingDao = ShoppingDatabase.getInstance(application).shoppingDao()
+    private val _shoppingList = mutableStateListOf<ShoppingItem>()
+    val shoppingList: List<ShoppingItem> get() = _shoppingList
+
+    init {
+        loadShoppingList()
+    }
+
+    private fun loadShoppingList() {
+        viewModelScope.launch(Dispatchers.IO) {
+        val items = dao.getALLItems()
+            _shoppingList.clear()
+            _shoppingList.addAll(items)
+        }
+    }
+
+
+    //    val shoppingList = mutableStateListOf(
+//        ShoppingItem("Молоко"),
+//        ShoppingItem("Хліб"),
+//        ShoppingItem("Яблука"),
+//        ShoppingItem("Олія"),
+//        ShoppingItem("Рис"),
+//    )
 
     fun toggleBought(index: Int) {
-        shoppingList[index] = shoppingList[index].copy(
-            isBought = !shoppingList[index].isBought)
+        viewModelScope.launch(Dispatchers.IO) {
+            val item = _shoppingList[index]
+            val updatedItem = item.copy(isBought = !item.isBought)
+            dao.updateItem(updatedItem)
+            _shoppingList[index] = updatedItem
+        }
     }
 }
 
@@ -102,11 +181,25 @@ fun ShoppingItemCard(
     }
 }
 
-@Composable
-fun ShoppingListScreen(viewModel: ShoppingListViewModel = viewModel()) {
+class ShoppingListViewModelFactory(private val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ShoppingListViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ShoppingListViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
 
+@Composable
+fun ShoppingListScreen(viewModel: ShoppingListViewModel = viewModel(
+    factory = ShoppingListViewModelFactory(LocalContext.current
+        .applicationContext as Application)
+)) {
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(16.dp)
     ) {
         itemsIndexed(viewModel.shoppingList) { ix, item ->
